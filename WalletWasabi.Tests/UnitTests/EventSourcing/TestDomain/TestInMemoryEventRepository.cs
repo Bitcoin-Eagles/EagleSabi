2a519d2bf06aa4d2674f51023ea049a5ae5293af
Bitcoin Eagle <hello@bitcoineagle.dev>
2021-12-16 19:41:26 +0400

    #10 Define critical section hooks in InMemoryEventRepository

--- WalletWasabi.Tests/UnitTests/EventSourcing/TestDomain/TestInMemoryEventRepository.cs
+++ WalletWasabi.Tests/UnitTests/EventSourcing/TestDomain/TestInMemoryEventRepository.cs
@@ -13,43 +13,54 @@ namespace WalletWasabi.Tests.UnitTests.EventSourcing.TestDomain
 
 		protected ITestOutputHelper Output { get; init; }
 
-		public SemaphoreSlim ValidatedSemaphore { get; } = new(0);
-		public SemaphoreSlim ConflictedSemaphore { get; } = new(0);
-		public SemaphoreSlim AppendedSemaphore { get; } = new(0);
+		public SemaphoreSlim Append_ValidatedSemaphore { get; } = new(0);
+		public SemaphoreSlim Append_MarkedUndeliveredSemaphore { get; } = new(0);
+		public SemaphoreSlim Append_ConflictedSemaphore { get; } = new(0);
+		public SemaphoreSlim Append_AppendedSemaphore { get; } = new(0);
 
-		public Action? ValidatedCallback { get; set; }
-		public Action? ConflictedCallback { get; set; }
-		public Action? AppendedCallback { get; set; }
+		public Action? Append_ValidatedCallback { get; set; }
+		public Action? Append_MarkedUndeliveredCallback { get; set; }
+		public Action? Append_ConflictedCallback { get; set; }
+		public Action? Append_AppendedCallback { get; set; }
 
 		protected override void Append_Validated()
 		{
 			base.Append_Validated();
 			Output.WriteLine(nameof(Append_Validated));
-			ValidatedSemaphore.Release();
-			ValidatedCallback?.Invoke();
+			Append_ValidatedSemaphore.Release();
+			Append_ValidatedCallback?.Invoke();
+		}
+
+		protected override void Append_MarkedUndelivered()
+		{
+			base.Append_MarkedUndelivered();
+			Output.WriteLine(nameof(Append_MarkedUndelivered));
+			Append_MarkedUndeliveredSemaphore.Release();
+			Append_MarkedUndeliveredCallback?.Invoke();
 		}
 
 		protected override void Append_Conflicted()
 		{
 			base.Append_Conflicted();
 			Output.WriteLine(nameof(Append_Conflicted));
-			ConflictedSemaphore.Release();
-			ConflictedCallback?.Invoke();
+			Append_ConflictedSemaphore.Release();
+			Append_ConflictedCallback?.Invoke();
 		}
 
 		protected override void Append_Appended()
 		{
 			base.Append_Appended();
 			Output.WriteLine(nameof(Append_Appended));
-			AppendedSemaphore.Release();
-			AppendedCallback?.Invoke();
+			Append_AppendedSemaphore.Release();
+			Append_AppendedCallback?.Invoke();
 		}
 
 		public void Dispose()
 		{
-			ValidatedSemaphore.Dispose();
-			ConflictedSemaphore.Dispose();
-			AppendedSemaphore.Dispose();
+			Append_ValidatedSemaphore.Dispose();
+			Append_MarkedUndeliveredSemaphore.Dispose();
+			Append_ConflictedSemaphore.Dispose();
+			Append_AppendedSemaphore.Dispose();
 		}
 	}
 }
