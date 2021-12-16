7588bba714d58a70777482ab5ddb8da66e3d29c2
Bitcoin Eagle <hello@bitcoineagle.dev>
2021-12-16 14:57:43 +0400

    #10 Finish second implementation attempt of list of undelivered events in InMemoryEventRepository

--- WalletWasabi/EventSourcing/Records/AggregateEvents.cs
+++ WalletWasabi/EventSourcing/Records/AggregateEvents.cs
@@ -2,18 +2,16 @@ using System.Collections.Immutable;
 
 namespace WalletWasabi.EventSourcing.Records
 {
-    public record AggregateEvents(long TailSequenceId, ImmutableList<WrappedEvent> Events, Guid TransactionId)
-    {
-        /// <summary>
-        /// SequenceId of the last event of this aggregate
-        /// </summary>
-        public long TailSequenceId { get; init; } = TailSequenceId;
+	public record AggregateEvents(long TailSequenceId, ImmutableList<WrappedEvent> Events)
+	{
+		/// <summary>
+		/// SequenceId of the last event of this aggregate
+		/// </summary>
+		public long TailSequenceId { get; init; } = TailSequenceId;
 
-        /// <summary>
-        /// Ordered list of events
-        /// </summary>
-        public ImmutableList<WrappedEvent> Events { get; init; } = Events;
-
-        public Guid TransactionId { get; init; } = TransactionId;
-    }
+		/// <summary>
+		/// Ordered list of events
+		/// </summary>
+		public ImmutableList<WrappedEvent> Events { get; init; } = Events;
+	}
 }
