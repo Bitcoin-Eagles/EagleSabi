900d1e2df93b46c806dedc3aa881be37ac4cb4cc
Bitcoin Eagle <hello@bitcoineagle.dev>
2021-12-21 15:45:40 +0400

    #10 inherit AggregateKey in AggregateUndeliveredEvents

--- WalletWasabi/EventSourcing/Records/UndeliveredEvent.cs
+++ WalletWasabi/EventSourcing/Records/UndeliveredEvent.cs
@@ -2,5 +2,6 @@ using System.Collections.Generic;
 
 namespace WalletWasabi.EventSourcing.Records
 {
-	public record AggregateUndeliveredEvents(string AggregateType, string AggregateId, IReadOnlyList<WrappedEvent> WrappedEvents);
+	public record AggregateUndeliveredEvents(string AggregateType, string AggregateId, IReadOnlyList<WrappedEvent> WrappedEvents)
+		: AggregateKey(AggregateType, AggregateId);
 }
