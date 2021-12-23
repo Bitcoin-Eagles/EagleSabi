d42c0eb7c99b0a2f39e772659978db6d78ac55c7
Bitcoin Eagle <hello@bitcoineagle.dev>
2021-12-23 15:48:10 +0400

    Convert EventStoreTests to async (#8)

--- WalletWasabi.Tests/UnitTests/EventSourcing/TestDomain/TestEventStore.cs
+++ WalletWasabi.Tests/UnitTests/EventSourcing/TestDomain/TestEventStore.cs
@@ -1,4 +1,5 @@
 using System.Threading;
+using System.Threading.Tasks;
 using WalletWasabi.EventSourcing;
 using WalletWasabi.EventSourcing.Interfaces;
 
@@ -18,30 +19,43 @@ namespace WalletWasabi.Tests.UnitTests.EventSourcing.TestDomain
 		public SemaphoreSlim PreparedSemaphore { get; } = new(0);
 		public SemaphoreSlim ConflictedSemaphore { get; } = new(0);
 		public SemaphoreSlim AppendedSemaphore { get; } = new(0);
+		public SemaphoreSlim PushedSemaphore { get; } = new(0);
 
-		public Action? PreparedCallback { get; set; }
-		public Action? ConflictedCallback { get; set; }
-		public Action? AppendedCallback { get; set; }
+		public Func<Task>? PreparedCallback { get; set; }
+		public Func<Task>? ConflictedCallback { get; set; }
+		public Func<Task>? AppendedCallback { get; set; }
+		public Func<Task>? PushedCallback { get; set; }
 
-		protected override void Prepared()
+		protected override async Task Prepared()
 		{
-			base.Prepared();
+			await base.Prepared();
 			PreparedSemaphore.Release();
-			PreparedCallback?.Invoke();
+			if (PreparedCallback is not null)
+				await PreparedCallback.Invoke();
 		}
 
-		protected override void Conflicted()
+		protected override async Task Conflicted()
 		{
-			base.Conflicted();
+			await base.Conflicted();
 			ConflictedSemaphore.Release();
-			ConflictedCallback?.Invoke();
+			if (ConflictedCallback is not null)
+				await ConflictedCallback.Invoke();
 		}
 
-		protected override void Appended()
+		protected override async Task Appended()
 		{
-			base.Appended();
+			await base.Appended();
 			AppendedSemaphore.Release();
-			AppendedCallback?.Invoke();
+			if (AppendedCallback is not null)
+				await AppendedCallback.Invoke();
+		}
+
+		protected override async Task Pushed()
+		{
+			await base.Pushed();
+			PushedSemaphore.Release();
+			if (PushedCallback is not null)
+				await PushedCallback.Invoke();
 		}
 
 		public void Dispose()
@@ -49,6 +63,12 @@ namespace WalletWasabi.Tests.UnitTests.EventSourcing.TestDomain
 			PreparedSemaphore.Dispose();
 			ConflictedSemaphore.Dispose();
 			AppendedSemaphore.Dispose();
+			PushedSemaphore.Dispose();
+
+			PreparedCallback = null;
+			ConflictedCallback = null;
+			AppendedCallback = null;
+			PushedCallback = null;
 		}
 	}
 }
